be369c57996df9c0d8ec4b709819c40af28b65d4
LYC386 <alex98605@gmail.com>
2023-08-17 16:11:13 +0800

    update simple account to generic

--- smartWallet/script/BLSAccountFactory.s.sol
+++ smartWallet/script/BLSAccountFactory.s.sol
@@ -6,7 +6,7 @@ import "../src/bls/BLSAccountFactory.sol";
 
 contract DeployAccountFactory is Script {
     //address entrypoint = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789; //entry point v0.6
-    address entrypoint = 0x3cD5A8Ddd0EFb5804978a4Da74D3Fb6d074829F3; //entry point v0.6
+    address entrypoint = 0x3cD5A8Ddd0EFb5804978a4Da74D3Fb6d074829F3; //local entry point
     address aggregator = address(0); //do not support aggregator for now
 
     //deploy bls account factory and bls account (when calling new BLSAccountFactory)
